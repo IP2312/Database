a32af98b327edc1a7fa96bc4e91747a98ce5cab1
Iason <iason.pifeas@gmail.com>
2025-05-06 21:05:37 +0200

    home3

--- SQL2/4_7-12 EmpDept query.sql
+++ SQL2/4_7-12 EmpDept query.sql
@@ -2,8 +2,6 @@ SELECT distinct DEPT.DEPTNO, DEPT.DNAME from DEPT,EMP where DEPT.DEPTNO = EMP.DE
 SELECT EMPNO, ENAME from EMP WHERE JOB = (Select JOB from EMP WHERE ENAME = 'JONES');
 SELECT EMPNO, ENAME, SAL from EMP WHERE SAL + NVL(COMM,0) > (Select avg(SAL + NVL(COMM,0)) from EMP where DEPTNO = 30);
 SELECT EMPNO, ENAME from EMP where sal + nvl(COMM,0) > (Select max(SAL + nvl(COMM,0)) from EMP where DEPTNO = 30);
-
 SELECT EMPNO, ENAME, JOB from EMP where DEPTNO = 10 and JOB not in (SELECT JOB from EMP where DEPTNO =30);
-
 SELECT EMPNO,ENAME, JOB, SAL from EMP where SAL = (select max(SAL) from  EMP);
 
