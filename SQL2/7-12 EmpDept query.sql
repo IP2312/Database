c9d4367c97af6fc9ed12b36dce191f971eae8117
Iason <iason.pifeas@gmail.com>
2025-05-06 14:35:17 +0200

    06.05

---
+++ SQL2/7-12 EmpDept query.sql
@@ -0,0 +1,5 @@
+SELECT distinct DEPT.DEPTNO, DEPT.DNAME from DEPT,EMP where DEPT.DEPTNO = EMP.DEPTNO (+) and  EMPNO is null;
+SELECT EMPNO, ENAME from EMP WHERE JOB = (Select JOB from EMP WHERE ENAME = 'JONES');
+SELECT EMPNO, ENAME, SAL from EMP WHERE SAL + NVL(COMM,0) > (Select avg(SAL + NVL(COMM,0)) from EMP where DEPTNO = 30);
+SELECT EMPNO, ENAME from EMP where sal + nvl(COMM,0) > (Select max(SAL + nvl(COMM,0)) from EMP where DEPTNO = 30);
+SELECT EMPNO, ENAME from EMP where DEPTNO = 10 and
